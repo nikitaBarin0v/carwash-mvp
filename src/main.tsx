0c516545555a4909015e7d1634f4872c3f30ad07
nikita <weblink.b@gmail.com>
2026-06-16 18:51:46 +0300

    fix: vite env types for ci

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error css import
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,15 +1,15 @@
 /// <reference types="vite/client" />
 
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
+  [key: string]: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
-}
-
-declare module '*.css' {
-  const content: string
-  export default content
 }
